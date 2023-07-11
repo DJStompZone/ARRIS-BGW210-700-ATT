038880c85b0b4a48f49fb25175b6191154e915cd
Russell Juma <32531681+RussellJuma@users.noreply.github.com>
2023-07-10 19:32:22 -0500

    Menu and tools added.

---
+++ BGW210/Menus.py
@@ -0,0 +1,14 @@
+from typing import Literal
+
+
+class DropdownOptions:
+    ethernet = Literal[
+        'Auto',
+        '100M full duplex',
+        '100M half duplex',
+        '10M full duplex',
+        '10M half duplex',
+        '1G full duplex',
+        '1G full duplex']
+    on_off = Literal['Off', 'On']
+    auto_on_off = Literal['Auto', 'Off', 'On']
--- BGW210/tools.py
+++ BGW210/tools.py
@@ -1,8 +1,43 @@
-from hashlib import md5
-password = 'qCjWmsEH20mktZHcwGvXb3vaxvPMprLv'
-nonce = '0356ea975ebc9d062129c99c23b1a614f1973003ffe9c3f2ae6f8ef26213a176'
-D = {'nonce': nonce,
-     'password': '*' * len(password),
-     'hashpassword': md5((password + nonce).encode('utf-8')).hexdigest(),
-     'Continue': 'Continue'}
-print(D)
+from typing import Tuple
+
+from bs4 import BeautifulSoup
+from requests import Response
+from bs4.element import ResultSet
+from Config import Config
+
+
+class Tools:
+    class Network:
+        @classmethod
+        def resolve_ip(cls) -> str:
+            pass
+
+        @classmethod
+        def resolve_secure(cls) -> str:
+            if True:
+                return 'http://'
+            else:
+                return 'https://'
+
+        @classmethod
+        def set_credentials(cls) -> Tuple[str, str, str]:
+            if True:
+                credentials = Config.BGW210.Credentials.Remote
+            else:
+                credentials = Config.BGW210.Credentials.Remote
+            return credentials.url, credentials.username, credentials.password
+
+        def login_required(self) -> bool:
+            pass
+
+        # if html_body.find('title').text == 'Login':
+
+    class Parser:
+        @staticmethod
+        def get_nonce(response: Response) -> str:
+            return BeautifulSoup(response.content, features="html.parser").find('input').attrs.get('value')
+
+        @staticmethod
+        def get_table_data(response: Response) -> ResultSet:
+            rows = BeautifulSoup(response.content, features="html.parser").findAll('tr')
+            return rows
