29ddf6e016fd0e3d8aa7391c868f151d438c2a3d
Russell Juma <32531681+RussellJuma@users.noreply.github.com>
2023-07-10 19:31:37 -0500

    requirements, setup, and runtime added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ARRIS-BGW210-700-ATT',
+    version='',
+    packages=['BGW210'],
+    url='',
+    license='',
+    author='Russell Juma',
+    author_email='RussellJuma@gmail.com',
+    description=''
+)
